b9f13889e613e2e67ab5d35abb58463f0385df37
Ben Simpson <b.albertsimpson@gmail.com>
2020-07-17 13:29:36 -0700

    updates to functioning

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,16 @@
-FROM python:3
-ENV PYTHONUNBUFFERED 1
-RUN mkdir /code
-WORKDIR /code
-COPY requirements.txt /code/
+FROM python:3.8
+
+RUN apt-get update \
+    && apt-get install -y --no-install-recommends \
+    && rm -rf /var/lib/apt/lists/*
+
+WORKDIR /usr/src/app
+COPY requirements.txt ./
 RUN pip install -r requirements.txt
-COPY . /code/
+COPY . .
+# added this to run bash commands
+RUN chmod +x entrypoint.sh
+RUN chmod +x wait-for-it.sh
+EXPOSE 8000
+# CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+#ENTRYPOINT ["sh", "entrypoint.sh"]
--- Pets_Site/settings.py
+++ Pets_Site/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dotenv
+
+dotenv.load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -25,7 +27,7 @@ SECRET_KEY = '$-rc#k_)xhayyhm+#1-2l_!r2%vhv#6-=j@e5xtafvjm)w6htn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['0.0.0.0', 'bjamin.tech']
 
 
 # Application definition
@@ -78,10 +80,15 @@ WSGI_APPLICATION = 'Pets_Site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
+
+        'NAME': os.getenv('POSTGRES_DB'),
+
+        'USER': os.getenv('POSTGRES_USER'),
+
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        
+        'HOST': os.getenv('POSTGRES_HOST'),
+
         'PORT': 5432,
     }
 }
